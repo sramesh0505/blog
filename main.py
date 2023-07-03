acfac28120911c344973512f11effbe4d930110a
Ramesh Shankar <sramesh0505@gmail.com>
2023-07-03 21:46:02 +0530

    error custom pages

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from flask import Flask,redirect,render_template,url_for
-
-
-
 app=Flask(__name__)
 
 @app.route("/")
@@ -9,6 +6,14 @@ def index():
     return "hello"
 
 
+
+#error pages samples
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404error.html"),404
+@app.errorhandler(500)
+def internal_error(error):
+    return render_template("500error.html"),500
 
 if __name__=="__main__":
     app.run(debug=True)
